6912faa02beb0c2420dda0166c9a40d6d33b0576
aleyaamarzuki <95844708+aleyaamarzuki@users.noreply.github.com>
2025-10-08 16:27:40 +0000

    Deploying to gh-pages from @ aleyaamarzuki/aleyaamarzuki.github.io@db02398816b3111de3c7a4ccc9b2f655409872f3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "cv",
-          description: "See highlights from my CV here. Otherwise you can view the full version by selecting the PDF button.",
+          description: "See my CV here.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
