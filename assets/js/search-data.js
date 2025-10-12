7f10cfc7a847546b19b457778972cedc72c583ff
aleyaamarzuki <95844708+aleyaamarzuki@users.noreply.github.com>
2025-10-12 13:54:52 +0000

    Deploying to gh-pages from @ aleyaamarzuki/aleyaamarzuki.github.io@7d737dfd05e8493075fa1dd8487da9a66d2922cb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -56,16 +56,11 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-the-impact-of-ocd-on-learning-and-decision-making-in-adolescence",
+          title: 'The impact of OCD on learning and decision-making in adolescence',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
