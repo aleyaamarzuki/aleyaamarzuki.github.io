a89d6fb243cbb491e45498d61c9cca2455fb602d
aleyaamarzuki <95844708+aleyaamarzuki@users.noreply.github.com>
2025-10-30 17:11:37 +0000

    Deploying to gh-pages from @ aleyaamarzuki/aleyaamarzuki.github.io@f1e25a4ce9610f187bfa0bf09e3750a69ecf26d7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -61,6 +61,11 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-scientists4palestine",
+          title: 'Scientists4Palestine',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
